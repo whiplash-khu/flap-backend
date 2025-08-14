d0c9d95ee5b24bed0280763a323916249998d1e2
seoeunbi25 <seoeunbi25@naver.com>
2025-08-14 15:56:01 +0900

    feat: Add chat schemas

---
+++ source/schemas/chatMessage.ts
@@ -0,0 +1,12 @@
+import { ChatMessage } from "@library/type";
+import S, { JSONSchema} from 'fluent-json-schema';
+import commonSchema from "./common";
+
+export default {
+    id: commonSchema['id'],
+    chatId: commonSchema['id'],
+    userId: commonSchema['id'],
+    content: S.string()
+        .minLength(1)
+        .maxLength(320)
+} satisfies Record<keyof Omit<ChatMessage, 'createdAt'>, JSONSchema>;
---
+++ source/schemas/chatUser.ts
@@ -0,0 +1,8 @@
+import { ChatUser } from "@library/type";
+import S, { JSONSchema} from 'fluent-json-schema';
+import commonSchema from "./common";
+
+export default {
+    chatId: commonSchema['id'],
+    userId: commonSchema['id'],
+} satisfies Record<keyof ChatUser, JSONSchema>;
