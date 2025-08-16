053fee24f8e8f0e383783a4745c9fbb07af1fecf
H2Owater425 <h2o@dhmo.kr>
2025-08-16 21:06:30 +0900

    refactor: Use schema to validate request
    refactor: Simplify logic
    feat: Fix controller name

--- source/routes/chats/chats.module.ts
+++ source/routes/chats/chats.module.ts
@@ -1,39 +1,50 @@
-import Module from '@library/module';import S from "fluent-json-schema";
-import postChatController from "./postChat.controller";
-import getChatController from "./getChat.controller";
-import postChatMessageController from "./postChatMessage.controller";
-import getChatMessageController from "./getChatMessage.controller";
-import chat from "@schemas/chat";
-import chatMessage from '@schemas/chatMessage';
+import Module from '@library/module';
+import S from 'fluent-json-schema';
+import postChatsController from './postChats.controller';
+import getChatsController from './getChats.controller';
+import postChatMessagesController from './postChatMessages.controller';
+import getChatMessagesController from './getChatMessages.controller';
+import chatSchema from '@schemas/chat';
+import chatMessageSchema from '@schemas/chatMessage';
+import pagenationSchema from '@schemas/pagenation';
+import chatUserSchema from '@schemas/chatUser';
 
 export default new Module('chats', [{
 	method: 'POST',
 	url: '',
-	handler: postChatController,
+	handler: postChatsController,
 	schema: {
 		body: S.object()
-			.prop('name', chat['name'].required())
+			.prop('name', chatSchema['name'].required())
+			.prop('userIds', S.array()
+				.items(chatUserSchema['userId'])
+				.minItems(2)
+				.uniqueItems(true)
+				.required())
 	}
 }, {
 	method: 'GET',
 	url: '',
-	handler: getChatController,
+	handler: getChatsController,
 }, {
 	method: 'POST',
 	url: ':chatId/messages',
-	handler: postChatMessageController,
+	handler: postChatMessagesController,
 	schema: {
 		params: S.object()
-			.prop('chatId', chat['id'].required()),
+			.prop('chatId', chatSchema['id'].required()),
 		body: S.object()
-			.prop('content', chatMessage['content'].required())
+			.prop('content', chatMessageSchema['content'].required())
 	}
 }, {
 	method: 'GET',
 	url: ':chatId/messages',
-	handler: getChatMessageController,
+	handler: getChatMessagesController,
 	schema: {
 		params: S.object()
-			.prop('chatId', chat['id'].required())
+			.prop('chatId', chatSchema['id'].required()),
+		querystring: S.object()
+			.prop('index', pagenationSchema['index'])
+			.prop('size', pagenationSchema['size'])
 	}
-}])
+}]);
---
+++ source/routes/chats/getChatMessages.controller.ts
@@ -0,0 +1,70 @@
+import { emptySelection } from '@library/constant';
+import { kysely } from '@library/database';
+import { NotFound } from '@library/httpError';
+import { Chat, ChatMessage, Database, Media, Pagenation, User } from '@library/type';
+import { FastifyRequest, FastifyReply } from 'fastify';
+import { SelectQueryBuilder, Transaction } from 'kysely';
+
+export default function(request: FastifyRequest<{
+	Params: {
+		chatId: Chat['id'];
+	};
+	Querystring: Pagenation;
+}>, reply: FastifyReply): Promise<void> {
+	return kysely.transaction()
+		.setAccessMode('read only')
+		.setIsolationLevel('repeatable read')
+		.execute(function (transaction: Transaction<Database>): Promise<void> {
+			return transaction.selectFrom('chat_user')
+				.select(emptySelection)
+				.where('chat_id', '=', request['params']['chatId'])
+				.where('user_id', '=', request['userId'])
+				.limit(1)
+				.executeTakeFirst()
+				.then(function (chatUser?: {}): Promise<Pick<ChatMessage & User & Media, 'id' | 'userId' | 'content' | 'createdAt' | 'name' | 'mediaId' | 'hash' | 'type'>[]> {
+					if(chatUser === undefined) {
+						throw new NotFound('Params["chatId"] must be valid')
+					}
+
+					return transaction.selectFrom('chat_message')
+						.select(['chat_message.id', 'chat_message.id as userId', 'chat_message.content', 'chat_message.created_at as createdAt'])
+						.innerJoin('user', 'chat_message.user_id', 'user.id')
+						.select(['user.name', 'user.media_id as mediaId'])
+						.innerJoin('media', 'user.media_id', 'media.id')
+						.select(['media.hash', 'media.type'])
+						.where('chat_id', '=', request['params']['chatId'])
+						.$if(typeof request['query']['index'] === 'number', function (queryBulder: SelectQueryBuilder<Database, "chat_message" | "media" | "user", Pick<ChatMessage & User & Media, 'id' | 'userId' | 'content' | 'createdAt' | 'name' | 'mediaId' | 'hash' | 'type'>>): SelectQueryBuilder<Database, "chat_message" | "media" | "user", Pick<ChatMessage & User & Media, 'id' | 'userId' | 'content' | 'createdAt' | 'name' | 'mediaId' | 'hash' | 'type'>> {
+							return queryBulder.where('chat_message.id', '<', request['query']['index'] as number);
+						})
+						.orderBy('chat_message.id', 'desc')
+						.limit(request['query']['size'])
+						.execute();
+				})
+				.then(function (_chatMessages: Pick<ChatMessage & User & Media, 'id' | 'userId' | 'content' | 'createdAt' | 'name' | 'mediaId' | 'hash' | 'type'>[]): void {
+					const chatMessages: (Pick<ChatMessage, 'id' | 'content' | 'createdAt'> & {
+						user: Pick<User, 'id' | 'name'> & {
+							media: Pick<Media, 'id' | 'hash' | 'type'>;
+						};
+					})[] = [];
+
+					for(let i: number = 0; i < _chatMessages['length']; i++) {
+						chatMessages.push({
+							id: _chatMessages[i]['id'],
+							content: _chatMessages[i]['content'],
+							createdAt: _chatMessages[i]['createdAt'],
+							user: {
+								id: _chatMessages[i]['userId'],
+								name: _chatMessages[i]['name'],
+								media: {
+									id: _chatMessages[i]['mediaId'],
+									hash: _chatMessages[i]['hash'],
+									type: _chatMessages[i]['type'],
+								}
+							}
+						});
+					}
+
+					reply.send(chatMessages);
+				});
+		});
+}
