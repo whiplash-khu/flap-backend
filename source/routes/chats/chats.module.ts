e2dce10bc35885d0eb23a5cce13941502d2e490b
H2Owater425 <h2o@dhmo.kr>
2025-08-17 16:54:50 +0900

    refactor: Fix array indent style

--- source/routes/chats/chats.module.ts
+++ source/routes/chats/chats.module.ts
@@ -6,21 +6,23 @@ import chatSchema from '@schemas/chat';
 import chatUserSchema from '@schemas/chatUser';
 import chatMessagesModule from './messages/chatMessages.module';
 
-export default new Module('chats', [{
-	method: 'POST',
-	url: '',
-	handler: postChatsController,
-	schema: {
-		body: S.object()
-			.prop('name', chatSchema['name'].required())
-			.prop('userIds', S.array()
-				.items(chatUserSchema['userId'])
-				.minItems(2)
-				.uniqueItems(true)
-				.required())
+export default new Module('chats', [
+	{
+		method: 'POST',
+		url: '',
+		handler: postChatsController,
+		schema: {
+			body: S.object()
+				.prop('name', chatSchema['name'].required())
+				.prop('userIds', S.array()
+					.items(chatUserSchema['userId'])
+					.minItems(2)
+					.uniqueItems(true)
+					.required())
+		}
+	}, {
+		method: 'GET',
+		url: '',
+		handler: getChatsController,
 	}
-}, {
-	method: 'GET',
-	url: '',
-	handler: getChatsController,
-}], [chatMessagesModule]);
+], [chatMessagesModule]);
--- source/routes/chats/messages/chatMessages.module.ts
+++ source/routes/chats/messages/chatMessages.module.ts
@@ -5,25 +5,27 @@ import getChatMessagesController from './getChatMessages.controller';
 import chatMessageSchema from '@schemas/chatMessage';
 import pagenationSchema from '@schemas/pagenation';
 
-export default new Module('messages', [{
-	method: 'POST',
-	url: ':chatId/messages',
-	handler: postChatMessagesController,
-	schema: {
-		params: S.object()
-			.prop('chatId', chatMessageSchema['chatId'].required()),
-		body: S.object()
-			.prop('content', chatMessageSchema['content'].required())
+export default new Module('messages', [
+	{
+		method: 'POST',
+		url: ':chatId/messages',
+		handler: postChatMessagesController,
+		schema: {
+			params: S.object()
+				.prop('chatId', chatMessageSchema['chatId'].required()),
+			body: S.object()
+				.prop('content', chatMessageSchema['content'].required())
+		}
+	}, {
+		method: 'GET',
+		url: ':chatId/messages',
+		handler: getChatMessagesController,
+		schema: {
+			params: S.object()
+				.prop('chatId', chatMessageSchema['chatId'].required()),
+			querystring: S.object()
+				.prop('index', pagenationSchema['index'])
+				.prop('size', pagenationSchema['size'])
+		}
 	}
-}, {
-	method: 'GET',
-	url: ':chatId/messages',
-	handler: getChatMessagesController,
-	schema: {
-		params: S.object()
-			.prop('chatId', chatMessageSchema['chatId'].required()),
-		querystring: S.object()
-			.prop('index', pagenationSchema['index'])
-			.prop('size', pagenationSchema['size'])
-	}
-}]);
+]);
