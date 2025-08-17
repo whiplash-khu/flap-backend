03004564787ac4d6d2c3fd1a4c76c9e1c3def957
H2Owater425 <h2o@dhmo.kr>
2025-08-17 16:53:09 +0900

    refactor: Add chatMessages module

--- source/routes/chats/chats.module.ts
+++ source/routes/chats/chats.module.ts
@@ -2,12 +2,9 @@ import Module from '@library/module';
 import S from 'fluent-json-schema';
 import postChatsController from './postChats.controller';
 import getChatsController from './getChats.controller';
-import postChatMessagesController from './postChatMessages.controller';
-import getChatMessagesController from './getChatMessages.controller';
 import chatSchema from '@schemas/chat';
-import chatMessageSchema from '@schemas/chatMessage';
-import pagenationSchema from '@schemas/pagenation';
 import chatUserSchema from '@schemas/chatUser';
+import chatMessagesModule from './messages/chatMessages.module';
 
 export default new Module('chats', [{
 	method: 'POST',
@@ -26,25 +23,4 @@ export default new Module('chats', [{
 	method: 'GET',
 	url: '',
 	handler: getChatsController,
-}, {
-	method: 'POST',
-	url: ':chatId/messages',
-	handler: postChatMessagesController,
-	schema: {
-		params: S.object()
-			.prop('chatId', chatSchema['id'].required()),
-		body: S.object()
-			.prop('content', chatMessageSchema['content'].required())
-	}
-}, {
-	method: 'GET',
-	url: ':chatId/messages',
-	handler: getChatMessagesController,
-	schema: {
-		params: S.object()
-			.prop('chatId', chatSchema['id'].required()),
-		querystring: S.object()
-			.prop('index', pagenationSchema['index'])
-			.prop('size', pagenationSchema['size'])
-	}
-}]);
+}], [chatMessagesModule]);
