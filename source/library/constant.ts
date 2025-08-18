cd1421742f332c25d1e9792c871ab1c150dba6ff
H2Owater425 <h2o@dhmo.kr>
2025-08-19 08:42:05 +0900

    refactor: Remove column alias

--- source/library/constant.ts
+++ source/library/constant.ts
@@ -111,4 +111,5 @@ export const LOST_PASSWORD_TEMPLATE: string = `<body style="margin:100px auto;wi
 
 export const TAG_REGULAR_EXPRESSION: RegExp = /(?<=#)[a-zA-Z0-9ㄱ-ㅣ가-힣_]{1,16}/g;
 
-export const emptySelection: AliasedRawBuilder<number, '1'> = sql<number>`1`.as('1');
+// @ts-expect-error
+export const emptySelection: AliasedRawBuilder<number, '1'> = sql<number>`1`;
