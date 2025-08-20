e93ca3c8e2b52810aba1f0aa48217fd1f57a9a79
seoeunbi25 <seoeunbi25@naver.com>
2025-08-20 20:07:32 +0900

    feat: Add postReactions schemas

---
+++ source/schemas/postReaction.ts
@@ -0,0 +1,11 @@
+import S, { JSONSchema } from 'fluent-json-schema';
+import { PostReaction } from '@library/type';
+import postSchema from './post';
+import userSchema from './user';
+
+export default {
+    postId: postSchema['id'],
+    userId: userSchema['id'],
+    emoji: S.string()
+        .maxLength(8)
+} satisfies Record<keyof PostReaction, JSONSchema>;
